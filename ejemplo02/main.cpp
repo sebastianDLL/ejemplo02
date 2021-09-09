a9f3020a27552f9eab44f11137661b37cafc59ba
HP <HP@DESKTOP-DGQ16R9>
2021-09-09 02:58:06 -0400

    Agregar archivos de proyecto.

---
+++ ejemplo02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nombre: Sebastian Delgadillo Llanos" << endl;
+	cout << "Carrera: Ingenieria de Sistemas" << endl;
+	cout << "CU: 35-5092 " << endl;
+	cout << "CI: 10643227" << endl;
+
+	return 0;
+}
